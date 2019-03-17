911aa53abb8d9f0b275f2481cafdb2da841050c8
Yusuf Yalim <yusufyalim@gmail.com>
2019-03-17 21:21:52 +0300

    Add NewDeckPage components

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import DeckListPage from './components/DeckListPage'
+import NewDeckPage from './components/NewDeckPage'
 
 export default class App extends React.Component {
   render() {
     return (
-      <DeckListPage />
+      <View>
+        <DeckListPage />
+        <NewDeckPage />
+      </View>
     );
   }
 }
---
+++ components/NewDeckPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const DeckListPage = () => (
+  <View>
+    <Text>NewDeckPage</Text>
+  </View>
+)
+
+export default DeckListPage
