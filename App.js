f816fd76c6d516f88e73f0aabea88c87d4e41e61
Yusuf Yalim <yusufyalim@gmail.com>
2019-03-17 21:13:24 +0300

    Add DeckListPage component

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import DeckListPage from './components/DeckListPage'
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <DeckListPage />
     );
   }
 }
