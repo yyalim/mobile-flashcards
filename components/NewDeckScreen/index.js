ac7f161f1f4230a92db0591a9c6cf0ca05bc666f
Yusuf Yalim <yusufyalim@gmail.com>
2019-03-18 22:30:57 +0300

    Add create new deck form

--- components/NewDeckScreen/index.js
+++ components/NewDeckScreen/index.js
@@ -1,10 +1,68 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet
+} from 'react-native'
 
-const DeckListScreen = () => (
-  <View>
-    <Text>NewDeckScreen</Text>
-  </View>
-)
+import { gray, blue, white } from '../../utils/colors'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  label: {
+    margin: 30,
+    textAlign: 'center',
+    fontSize: 24
+  },
+  textInput: {
+    borderColor: gray,
+    borderWidth: 1,
+    marginTop: 0,
+    margin: 30,
+    padding: 3,
+    width: 300
+  }
+})
+
+class DeckListScreen extends Component {
+  state = {
+    deckName: ''
+  }
+
+  handleChangeText = deckName => {
+    this.setState({ deckName })
+  }
+
+  handleSubmit = () => {
+    console.log(this.state.deckName)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text
+          style={styles.label}
+        >
+          What is the title of our new deck?
+        </Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={this.handleChangeText}
+          value={this.state.deckName}
+        />
+        <Button
+          title="Create Deck"
+          onPress={this.onPress}
+        />
+      </View>
+    )
+  }
+}
 
 export default DeckListScreen
